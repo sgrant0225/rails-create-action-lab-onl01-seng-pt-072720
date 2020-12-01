a508d2abfa626d4cb17e0cda8af0c762b3ed0a77
Sharnell Grant <sharnellgrant@gmail.com>
2020-12-01 15:03:00 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,11 @@ class StudentsController < ApplicationController
   def new
   end
 
-  def create
+  def create #creates a new student
+   @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to student_path(@student) #redirects to the new student 
   end
-
 end
